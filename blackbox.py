8863f8fd6049f5f0bffce5748c9019734953a544
Sophie Stephenson <sophia.r.stephenson@gmail.com>
2021-11-17 12:19:39 -0600

    start blackbox

---
+++ blackbox.py
@@ -0,0 +1,39 @@
+################################
+# blackbox.py
+################################
+
+import cv2 as cv
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', type=str, help='Path to the video', default='cam1.mov')
+args = parser.parse_args()
+
+backSub = cv.createBackgroundSubtractorKNN()
+
+capture = cv.VideoCapture(cv.samples.findFileOrKeep('cubes/recordings/' + args.f))
+if not capture.isOpened():
+	print("Unable to open", args.f)
+	exit(0)
+
+while True:
+	ret, frame = capture.read()
+	if frame is None:
+		break
+
+	fgMask = backSub.apply(frame)
+
+	cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+	cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+				cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+	
+	
+	cv.imshow('Frame', frame)
+	cv.imshow('FG Mask', fgMask)
+	
+	keyboard = cv.waitKey(30)
+	if keyboard == 'q' or keyboard == 27:
+		break
+
+
+
